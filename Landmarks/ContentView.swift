739372fed6820ac133adeb3ff6b59f96dcbab360
Noah Ueffing <noahueffing@gmail.com>
2023-09-24 11:36:19 -0300

    Initial commit

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    var Font = "arial"
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, Norris!")
+                .font(.title)
+                .foregroundColor(.red)
         }
         .padding()
     }
