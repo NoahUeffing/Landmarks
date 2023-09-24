4faf6cf7d1c250a14f6fd5dc9fdb409cbaf31f6b
NoahUeffing <noahueffing@gmail.com>
2023-09-24 12:07:14 -0300

    Complete Creating and combining views

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -11,14 +11,35 @@ struct ContentView: View {
     var Font = "arial"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Norris!")
-                .font(.title)
-                .foregroundColor(.red)
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.black)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive test goees here.")
+            }
+            
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
@@ -27,3 +48,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Stopped at Section 3
+// Combine Views Using Stacks
+// https://developer.apple.com/tutorials/swiftui/creating-and-combining-views
